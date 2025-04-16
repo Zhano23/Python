bb3926b2820dbe884ea7d9f3508eb15a16521c9d
Jalal <125566382+Zhano23@users.noreply.github.com>
2025-04-16 12:50:48 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import datetime;
-
-from datetime import date, time, datetime
-date(year=2021, month=10, day=22)
-datetime.date(2021, 10, 22)
-time(hour=10, minute=19, second=10)
-datetime.time(10, 19, 10)
-datetime(year=2021, month=10, day=22, hour=10, minute=19, second=10)
-datetime(2021, 10, 22, 10, 19, 10)
